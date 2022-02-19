0d3a132084b2bf1d72e65d05ec46b74ac23d60d7
wanabanda <wanalbanda@gmail.com>
2022-02-19 16:30:27 +0000

    recipes show

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,14 @@ import React, { useState, Fragment } from "react";
 //import axios from "axios";
 import Search from "../components/Search";
 import RecipeList from "../components/RecipeList";
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Home = () => {
   const [items, setItems] = useState([]);
   const [term, setTerm] = useState("");
   const [saved, setSaved] = useState([]);
   //const authToken = localStorage.getItem("token");
-
+  //console.log(API_KEY);
   //search(): Searches the API for the terms entered by the user
   /*useEffect(() => {
     const axiosData = async () => {
@@ -30,7 +30,8 @@ const Home = () => {
 
   //search(): Searches the iTunes API for the terms entered by the user
   async function search(value) {
-    const url = ` https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}`;
+    const url = `https://api.spoonacular.com/recipes/complexSearch?lime&number=100&apiKey=${API_KEY}`;
+
     const results = await fetch(url).then((res) => res.json());
     const savedIds = saved.map((item) => item.id);
     if (!results.error) {
